91a97633044aa3d3863c5517bee97c3d348d4ada
Pavlo Suikov <pavlo.suikov@ti.com>
2013-02-11 21:05:34 +0200

    hwc: Skip blitter composition in any dual display mode
    We need to disable blitter in any dual display scenario,
    because we don't know how blitter behaves with two FBs.
    
    Change-Id: I90e46195fa1b8692c3f7e419619898716c1a71b3
    Signed-off-by: Pavlo Suikov <pavlo.suikov@ti.com>
    Signed-off-by: D. Andrei Măceș <dmaces@nd.edu>

--- hwc/blitter.c
+++ hwc/blitter.c
@@ -93,7 +93,7 @@ bool blit_layers(omap_hwc_device_t *hwc_dev, hwc_display_contents_1_t *contents,
     /* Do not blit if this frame will be composed entirely by the GPU.
      * Currently blitter is supported only for single display scenarios.
      */
-    if (!contents || hwc_dev->force_sgx || is_external_display_mirroring(hwc_dev))
+    if (!contents || hwc_dev->force_sgx || hwc_dev->displays[HWC_DISPLAY_EXTERNAL])
         goto err_out;
 
     blitter_config_t *blitter = &hwc_dev->blitter;
