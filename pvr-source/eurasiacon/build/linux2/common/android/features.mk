8010017581febc763b8fb032322a868dbca400c0
Jon Lee <athens69@att.net>
2015-07-14 15:06:28 -0400

    pvr-source: Updated build configuration
    Change-Id: I219e99c203b75347bb6996c50a1cb9174ac38f33

--- pvr-source/eurasiacon/build/linux2/common/android/features.mk
+++ pvr-source/eurasiacon/build/linux2/common/android/features.mk
@@ -43,7 +43,7 @@ include ../common/android/platform_version.mk
 # Basic support option tuning for Android
 #
 SUPPORT_ANDROID_PLATFORM := 1
-SUPPORT_OPENGLES1_V1_ONLY := 1
+SUPPORT_OPENGLES1_V1_ONLY := 0
 
 # Meminfo IDs are required for buffer stamps
 #
@@ -100,6 +100,12 @@ SUPPORT_LARGE_GENERAL_HEAP := 1
 PVR_LINUX_MEM_AREA_POOL_MAX_PAGES ?= 5400
 
 ##############################################################################
+# Framebuffer target extension is used to find configs compatible with
+# the framebuffer (added in JB MR1).
+#
+EGL_EXTENSION_ANDROID_FRAMEBUFFER_TARGET := 1
+
+##############################################################################
 # EGL connect/disconnect hooks only available since Froyo
 # Obsolete in future versions
 #
@@ -140,12 +146,11 @@ UNITTEST_INCLUDES += \
  $(ANDROID_ROOT)/frameworks/native/include \
  $(ANDROID_ROOT)/frameworks/native/opengl/include \
  $(ANDROID_ROOT)/libnativehelper/include
-# FIXME: This is the old location for the JNI header.
-UNITTEST_INCLUDES += $(ANDROID_ROOT)/dalvik/libnativehelper/include
+UNITTEST_INCLUDES += $(ANDROID_ROOT)/dalvik/libnativehelper
 else
 UNITTEST_INCLUDES += \
  $(ANDROID_ROOT)/frameworks/base/opengl/include \
- $(ANDROID_ROOT)/dalvik/libnativehelper/include
+ $(ANDROID_ROOT)/dalvik/libnativehelper/include/nativehelper
 endif
 
 # But it doesn't have OpenVG headers
@@ -317,6 +322,110 @@ endif
 #
 ifeq ($(is_at_least_jellybean),1)
 PVR_ANDROID_HAS_CORKSCREW_API := 1
+endif
+
+##############################################################################
+# JB MR1 makes the framebuffer HAL obsolete.
+#
+# We also need to support IMPLEMENTATION_DEFINED so gralloc allocates
+# framebuffers and GPU buffers in a 'preferred' format.
+#
+ifeq ($(is_at_least_jellybean_mr1),0)
+SUPPORT_ANDROID_FRAMEBUFFER_HAL := 1
+else
+PVR_ANDROID_HAS_HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED := 1
+endif
+
+##############################################################################
+# JB MR1 introduces cross-process syncs associated with a fd.
+# This requires a new enough kernel version to have the base/sync driver.
+#
+ifeq ($(is_at_least_jellybean_mr1),1)
+EGL_EXTENSION_ANDROID_NATIVE_FENCE_SYNC := 0
+PVR_ANDROID_NATIVE_WINDOW_HAS_SYNC := 0
+endif
+
+##############################################################################
+# JB MR1 introduces new usage bits for the camera HAL and some new formats.
+#
+ifeq ($(is_at_least_jellybean_mr1),1)
+PVR_ANDROID_HAS_GRALLOC_USAGE_HW_CAMERA := 1
+PVR_ANDROID_HAS_HAL_PIXEL_FORMAT_RAW_SENSOR := 1
+PVR_ANDROID_HAS_HAL_PIXEL_FORMAT_BLOB := 1
+endif
+
+##############################################################################
+# JB MR2 adds a new graphics HAL (gralloc) API function, lock_ycbcr(), and
+# a so-called "flexible" YUV format enum.
+#
+ifeq ($(is_at_least_jellybean_mr2),1)
+PVR_ANDROID_HAS_HAL_PIXEL_FORMAT_YCbCr_420_888 := 1
+PVR_ANDROID_GRALLOC_HAS_0_2_FEATURES := 1
+endif
+
+##############################################################################
+# In JB MR2 we can use a native helper library for the unittest wrapper.
+# In earlier versions, we must use a less ideal approach.
+#
+ifeq ($(is_at_least_jellybean_mr2),0)
+PVR_ANDROID_SURFACE_FIELD_NAME := \"mNativeSurface\"
+endif
+
+##############################################################################
+# JB MR2 introduces two new camera HAL formats (Y8, Y16)
+#
+ifeq ($(is_at_least_jellybean_mr2),1)
+PVR_ANDROID_HAS_HAL_PIXEL_FORMAT_Y8 := 1
+PVR_ANDROID_HAS_HAL_PIXEL_FORMAT_Y16 := 1
+endif
+
+##############################################################################
+# KK's EGL wrapper remaps EGLConfigs in the BGRA and BGRX formats to RGBA and
+# RGBX respectively, for CpuConsumer compatibility. It does this because the
+# usage bits for the gralloc allocation are not available to EGL.
+#
+# In this newer platform version, gralloc has been redefined to allow the
+# 'format' parameter to gralloc->alloc() to be ignored for non-USAGE_SW
+# allocations, so long as the bits per channel and sRGB-ness are preserved.
+#
+ifeq ($(is_at_least_kitkat),1)
+PVR_ANDROID_REMAP_HW_ONLY_PIXEL_FORMATS := 1
+endif
+
+##############################################################################
+# Support newer HWC features in KK
+#
+ifeq ($(is_at_least_kitkat),1)
+PVR_ANDROID_HWC_HAS_1_3_FEATURES := 1
+endif
+
+##############################################################################
+# KK eliminated egl.cfg. Only create for older versions.
+#
+ifeq ($(is_at_least_kitkat),0)
+PVR_ANDROID_HAS_EGL_CFG := 1
+endif
+
+##############################################################################
+# KK has a bug in its browser that we need to work around.
+#
+ifeq ($(is_at_least_kitkat),1)
+PVR_ANDROID_RELAX_GRALLOC_MODULE_MAP_CHECKS := 1
+endif
+
+##############################################################################
+# KK's Camera HAL requires that ACTIVE_ARRAY_SIZE specify xmin/ymin first
+#
+ifeq ($(is_at_least_kitkat),1)
+PVR_ANDROID_CAMERA_ACTIVE_ARRAY_SIZE_HAS_XMIN_YMIN := 1
+endif
+
+##############################################################################
+# KitKat added a new memory tracking HAL. This enables gralloc support for
+# the GRAPHICS/GL memtrack types.
+#
+ifeq ($(is_at_least_kitkat),1)
+SUPPORT_ANDROID_MEMTRACK_HAL := 1
 endif
 
 # Placeholder for future version handling
