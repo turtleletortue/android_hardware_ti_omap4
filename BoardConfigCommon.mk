b2b43e0e332a58c82c9d19c6881985aa308e05eb
Kyle Repinski <repinski23@gmail.com>
2016-08-26 01:45:46 -0500

    Set TARGET_FORCE_SCREENSHOT_CPU_PATH.
    Change-Id: Ib73612f06bc41dc6270f2192c8c342ce2bae86f8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -26,6 +26,7 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 
 # Graphics
 USE_OPENGL_RENDERER := true
+TARGET_FORCE_SCREENSHOT_CPU_PATH ?= true
 
 # Global include changes
 TARGET_SPECIFIC_HEADER_PATH += $(OMAP4_NEXT_FOLDER)/include
