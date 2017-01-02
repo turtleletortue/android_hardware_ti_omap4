87bc87c2f668d19aeb8feffa8b88dd4c42cecccb
D. Andrei Măceș <dmaces@nd.edu>
2017-01-02 13:47:14 -0500

    pvr-km/init: Re-add support for DDK's omaplfb module
    Building only works with K3.4 unfortunately
    
    Change-Id: I9e90e2a4c3d23f288c9710a3e3b9add803815765

--- pvr-km.mk
+++ pvr-km.mk
@@ -14,13 +14,14 @@
 
 PVR_MODULES:
 	make clean -C $(OMAP4_NEXT_FOLDER)/pvr-source/eurasiacon/build/linux2/omap4430_android
+	cp $(TARGET_KERNEL_SOURCE)/drivers/video/omap2/omapfb/omapfb.h $(KERNEL_OUT)/drivers/video/omap2/omapfb/omapfb.h
 	make -j8 -C $(OMAP4_NEXT_FOLDER)/pvr-source/eurasiacon/build/linux2/omap4430_android \
 			ARCH=arm $(if $(ARM_CROSS_COMPILE),$(ARM_CROSS_COMPILE),$(KERNEL_CROSS_COMPILE)) \
 			TARGET_SGX=$(if $(filter-out 4470,$(TARGET_BOARD_OMAP_CPU)),540,544sc) \
 			KERNELDIR=$(KERNEL_OUT) BUILD=debug
-	mv $(KERNEL_OUT)/../../target/kbuild/pvrsrvkm.ko $(KERNEL_MODULES_OUT)
+	mv $(KERNEL_OUT)/../../target/kbuild/{pvrsrvkm,omaplfb}.ko $(KERNEL_MODULES_OUT)
 	$(if $(ARM_EABI_TOOLCHAIN),$(ARM_EABI_TOOLCHAIN)/arm-eabi-strip, \
 			$(KERNEL_TOOLCHAIN_PATH)strip) --strip-unneeded \
-			$(KERNEL_MODULES_OUT)/pvrsrvkm.ko
+			$(KERNEL_MODULES_OUT)/{pvrsrvkm,omaplfb}.ko
 
 TARGET_KERNEL_MODULES += PVR_MODULES
